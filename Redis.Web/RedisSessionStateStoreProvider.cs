64e60fa66035354375284ed0c27309dd65d1d0c4
Jack Cheng <jack.cheng@commercan.com>
2015-02-20 16:44:43 -0800

    work in progress

--- Redis.Web/RedisSessionStateStoreProvider.cs
+++ Redis.Web/RedisSessionStateStoreProvider.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Configuration;
+using System.IO;
 using System.Web;
 using System.Web.Configuration;
+using System.Web.Mvc;
 using System.Web.SessionState;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace Redis.Web
@@ -18,6 +21,7 @@ namespace Redis.Web
         public int Timeout { get; set; }
         public string SessionItems { get; set; }
         public bool Locked { get; set; }
+        public int Flags { get; set; }
     }
 
     public class RedisSessionStateStoreProvider : SessionStateStoreProviderBase
@@ -62,43 +66,119 @@ namespace Redis.Web
 
         public override SessionStateStoreData GetItem(HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
         {
-            throw new NotImplementedException();
+            return GetSessionStoreItem(false, context, id, out locked, out lockAge, out lockId, out actions);
         }
 
         public override SessionStateStoreData GetItemExclusive(HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
         {
-            throw new NotImplementedException();
+            return GetSessionStoreItem(true, context, id, out locked, out lockAge, out lockId, out actions);
         }
 
         public override void ReleaseItemExclusive(HttpContext context, string id, object lockId)
         {
-            throw new NotImplementedException();
+            var cached = Database.StringGet(id);
+            var item = DeserializeSessionItem(cached);
+
+            item.Locked = false;
+            item.Expires = DateTime.UtcNow.AddMinutes(_configSection.Timeout.TotalMinutes);
+            cached = SerializeSessionItem(item);
+
+            Database.StringSet(id, cached);
+            Database.KeyExpire(id, item.Expires);
         }
+
+       
 
         public override void SetAndReleaseItemExclusive(HttpContext context, string id, SessionStateStoreData item, object lockId, bool newItem)
         {
-            throw new NotImplementedException();
+            var items = Serialize((SessionStateItemCollection) item.Items);
+            string cached = string.Empty;
+            SessionItem sessionItem = null;
+
+            if (newItem)
+            {
+               var utcNow = DateTime.UtcNow;
+
+               sessionItem = new SessionItem
+                {
+                    SessionId = id,
+                    Created = utcNow,
+                    Expires = utcNow,
+                    LockDate = utcNow,
+                    LockId = 0,
+                    Timeout = item.Timeout,
+                    SessionItems = string.Empty,
+                    Flags = (int)SessionStateActions.None
+                };
+            }
+            else
+            {
+                cached = Database.StringGet(id);
+                sessionItem = DeserializeSessionItem(cached);
+                sessionItem.Expires = DateTime.UtcNow.AddMinutes(item.Timeout);
+                sessionItem.Locked = false;
+                sessionItem.SessionItems = items;
+                sessionItem.LockId = (int) lockId;
+               
+            }
+
+            cached = SerializeSessionItem(sessionItem);
+            Database.StringSet(id, cached);
+            Database.KeyExpire(id, sessionItem.Expires);
         }
 
         public override void RemoveItem(HttpContext context, string id, object lockId, SessionStateStoreData item)
         {
-            throw new NotImplementedException();
+            
+            var cached = Database.StringGet(id);
+            if (cached.IsNullOrEmpty)
+                return;
+            var sessionItem = DeserializeSessionItem(cached);
+            if (sessionItem.LockId == (int) lockId)
+                Database.KeyDelete(id);
         }
 
         public override void ResetItemTimeout(HttpContext context, string id)
         {
-            throw new NotImplementedException();
+            if (!Database.KeyExists(id))
+                return;
+
+            var cached = Database.StringGet(id);
+            var sessionItem = DeserializeSessionItem(cached);
+
+            sessionItem.Expires = DateTime.UtcNow.AddMinutes(_configSection.Timeout.TotalMinutes);
+            cached = SerializeSessionItem(sessionItem);
+
+            Database.StringSet(id, cached);
+            Database.KeyExpire(id, sessionItem.Expires);
         }
 
         public override SessionStateStoreData CreateNewStoreData(HttpContext context, int timeout)
         {
-            throw new NotImplementedException();
+            return new SessionStateStoreData(new SessionStateItemCollection(), 
+                SessionStateUtility.GetSessionStaticObjects(context),
+                timeout);
         }
 
         public override void CreateUninitializedItem(HttpContext context, string id, int timeout)
         {
-           
-            Database.KeyExpire(id, DateTime.UtcNow.AddMinutes(timeout));
+            var utcNow = DateTime.UtcNow;
+
+            var item = new SessionItem
+            {
+                SessionId = id,
+                Created = utcNow,
+                Expires = utcNow,
+                LockDate = utcNow,
+                LockId = 0,
+                Timeout = timeout,
+                SessionItems = string.Empty,
+                Flags = (int) SessionStateActions.None
+               
+            };
+            var serialized = SerializeSessionItem(item);
+            Database.StringSet(id, serialized);
+            Database.KeyExpire(id, utcNow.AddMinutes(timeout));
         }
 
         public override void EndRequest(HttpContext context)
@@ -113,14 +193,110 @@ namespace Redis.Web
             out object lockId,
             out SessionStateActions actionFlags)
         {
+            SessionItem cachedItem = null;
+            
             SessionStateStoreData item = null;
+            string sessionItems = null;
+            int timeout = 0;
+
             lockAge = TimeSpan.Zero;
             lockId = null;
             locked = false;
             actionFlags = SessionStateActions.None;
+            string cached = Database.StringGet(id);
+            var deleteData = false;
 
+            if (string.IsNullOrEmpty(cached))
+            {
+                locked = false;
+                return item;
+            }
 
+            cachedItem = JsonConvert.DeserializeObject<SessionItem>(cached);
+
+            if (lockRecord)
+            {
+                
+
+                if (cachedItem.Locked != true)
+                {
+                    cachedItem.Locked = true;
+                    cachedItem.LockDate = DateTime.UtcNow;
+
+                    cached = JsonConvert.SerializeObject(cachedItem);
+                    Database.StringSet(id, cached);
+                }
+
+                locked = true;
+            }
+
+            var expires = cachedItem.Expires;
+            if (expires < DateTime.UtcNow)
+            {
+                locked = false;
+            }
+
+            sessionItems = cachedItem.SessionItems;
+            lockId = cachedItem.LockId;
+            lockAge = DateTime.UtcNow.Subtract(cachedItem.LockDate);
+            actionFlags = (SessionStateActions) cachedItem.Flags;
+            timeout = cachedItem.Timeout;
+
+            if (!locked)
+            {
+                lockId = (int) lockId + 1;
+                cachedItem.LockId = (int) lockId;
+                cachedItem.Flags = (int) SessionStateActions.None;
+                cached = JsonConvert.SerializeObject(cachedItem);
+                Database.StringSet(id, cached);
+
+                if (actionFlags == SessionStateActions.InitializeItem)
+                    item = CreateNewStoreData(context, (int) _configSection.Timeout.TotalMinutes);
+                else
+                    item = Deserialize(context, sessionItems, timeout);
+            }
             return item;
+        }
+
+        private string Serialize(SessionStateItemCollection items)
+        {
+                using (MemoryStream ms = new MemoryStream())
+                using (BinaryWriter writer = new BinaryWriter(ms))
+                {
+                    if (items != null)
+                        items.Serialize(writer);
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+        }
+
+        private SessionStateStoreData Deserialize(HttpContext context,
+            string serializedItems, int timeout)
+        {
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(serializedItems)))
+            {
+                SessionStateItemCollection sessionItems = new SessionStateItemCollection();
+
+                if (ms.Length > 0)
+                {
+                    using (BinaryReader reader = new BinaryReader(ms))
+                    {
+                        sessionItems = SessionStateItemCollection.Deserialize(reader);
+                    }
+                }
+
+               return new SessionStateStoreData(sessionItems, SessionStateUtility.GetSessionStaticObjects(context),
+               timeout);
+            }
+        }
+
+        private static SessionItem DeserializeSessionItem(RedisValue cached)
+        {
+            return JsonConvert.DeserializeObject<SessionItem>(cached);
+        }
+
+        private static string SerializeSessionItem(SessionItem item)
+        {
+            return JsonConvert.SerializeObject(item);
         }
     }
 }
